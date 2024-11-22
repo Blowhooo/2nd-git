9bf1a9030907b437082e7a0ea5ef903f2af621b7
Blowhooo <blowhooo@gmail.com>
2024-11-23 01:28:05 +0900

    두번째 회원가입 폼 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
+
 const ctrl = require('./home.ctrl');
 
-router.get('/', ctrl.hello);
-router.get('/login', ctrl.login);
+router.get('/', ctrl.output.home);
+router.get('/login', ctrl.output.login);
+router.get('/register', ctrl.output.register);
+router.post('/login', ctrl.process.login);
 
 module.exports = router;
